23ee7ddc321cbb1c4b633601cde83644ab4496e1
Igor Sthanny <igor.nascimento@popcode.com.br>
2021-10-18 12:03:59 -0300

    Initial Commit

---
+++ swift_study/swift_studyApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_studyApp.swift
+//  swift_study
+//
+//  Created by Igor Sthaynny on 18/10/21.
+//
+
+import SwiftUI
+
+@main
+struct swift_studyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
